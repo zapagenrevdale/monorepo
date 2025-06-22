8891d27f19e8bd6637e1cc3ded99e3b860641aa4
genrev-zapa <zapagenrevdale@gmail.com>
2025-06-22 15:17:37 +0800

    hotfix: relative pathing

--- infra/sst.config.ts
+++ infra/sst.config.ts
@@ -57,52 +57,52 @@ export default $config({
       copyFiles: [
         {
           from: "../packages/db/node_modules/@prisma/client/runtime/",
-          to: "/node_modules/@prisma/client/runtime"
+          to: "node_modules/@prisma/client/runtime"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/client.js",
-          to: "/node_modules/@prisma/client/client.js"
+          to: "node_modules/@prisma/client/client.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/default.js",
-          to: "/node_modules/@prisma/client/default.js"
+          to: "node_modules/@prisma/client/default.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/edge.js",
-          to: "/node_modules/@prisma/client/edge.js"
+          to: "node_modules/@prisma/client/edge.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/extension.js",
-          to: "/node_modules/@prisma/client/extension.js"
+          to: "node_modules/@prisma/client/extension.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/index-browser.js",
-          to: "/node_modules/@prisma/client/index-browser.js"
+          to: "node_modules/@prisma/client/index-browser.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/index.js",
-          to: "/node_modules/@prisma/client/index.js"
+          to: "node_modules/@prisma/client/index.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/package.json",
-          to: "/node_modules/@prisma/client/package.json"
+          to: "node_modules/@prisma/client/package.json"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/react-native.js",
-          to: "/node_modules/@prisma/client/react-native.js"
+          to: "node_modules/@prisma/client/react-native.js"
         },
 
         {
           from: "../packages/db/node_modules/@prisma/client/sql.js",
-          to: "/node_modules/@prisma/client/sql.js"
+          to: "node_modules/@prisma/client/sql.js"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/sql.mjs",
-          to: "/node_modules/@prisma/client/sql.mjs"
+          to: "node_modules/@prisma/client/sql.mjs"
         },
         {
           from: "../packages/db/node_modules/@prisma/client/wasm.js",
-          to: "/node_modules/@prisma/client/wasm.js"
+          to: "node_modules/@prisma/client/wasm.js"
         },
 
 
